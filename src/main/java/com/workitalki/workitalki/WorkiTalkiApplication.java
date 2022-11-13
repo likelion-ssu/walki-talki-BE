2c16975347e0364a0a26a80aeb8d9a15ca8d1f8e
seongwon-ha <djfqks22@naver.com>
2022-11-13 22:51:16 +0900

    test

---
+++ src/main/java/com/workitalki/workitalki/WorkiTalkiApplication.java
@@ -0,0 +1,13 @@
+package com.workitalki.workitalki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkiTalkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkiTalkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workitalki/workitalki/WorkiTalkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workitalki.workitalki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkiTalkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
